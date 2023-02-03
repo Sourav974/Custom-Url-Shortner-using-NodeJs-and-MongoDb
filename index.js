35d6d8e01fa040f10953f75dcc12cab948d6018c
Sourav <sourav@192.168.1.6>
2023-02-04 00:35:35 +0530

    Added stateful Authentication using cookies session id

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const path = require("path");
+const cookieParser = require("cookie-parser");
+const { restrictToLoggedinUserOnly, checkAuth } = require("./middlewares/auth");
 
 const { connectToMongoDb } = require("./connect");
 const URL = require("./models/url");
+
 const urlRoute = require("./routes/url");
 const staticRoute = require("./routes/StaticRouter");
-const { urlencoded } = require("express");
+const userRoute = require("./routes/user");
+
 const app = express();
 const PORT = 8001;
 
@@ -18,14 +22,16 @@ app.set("views", path.resolve("./views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.get("/test", async (req, res) => {
   const allUrls = await URL.find({});
   return res.render("home");
 });
 
-app.use("/url", urlRoute);
-app.use("/", staticRoute);
+app.use("/url", restrictToLoggedinUserOnly, urlRoute);
+app.use("/", checkAuth, staticRoute);
+app.use("/user", userRoute);
 
 app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: "string",
+      required: true,
+    },
+
+    email: {
+      type: "string",
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: "string",
+      required: true,
+    },
+  },
+  { timestamp: true }
+);
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
